d5bbc54954ce042859f19e418a6ed1ebb8a0a8a8
marianorigo18 <marianowagnerrigo@gmail.com>
2022-11-29 16:37:06 -0300

    enviando peticiones de javascript a HTML

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-import {logos, topic} from '../exports/data.js'
+import {iluminacionT, industriaT} from '../exports/data.js'
+const containerIluminacion = document.querySelector('#card-iluminacion')
+const containerIndustria = document.querySelector('#card-industria')
 
-console.log(logos)
+let fragment = '';
+
+iluminacionT.forEach( elemet => {
+    fragment +=`
+    <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+        <div class="card-product">
+                <img src="${elemet.img}" class="card-img-top" alt="Imagen De Lampara">
+            <div class="card-body">
+                 <h5 class="card-title text-center fw-light">${elemet.title}</h5>
+            </div>
+        </div>
+    </div>
+    `
+})
+industriaT.forEach( elemet => {
+    fragment +=`
+    <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+    <div class="card-product">
+            <img src="${elemet.img}" class="card-img-top" alt="Imagen De Lampara">
+        <div class="card-body">
+             <h5 class="card-title text-center fw-light">${elemet.title}</h5>
+        </div>
+    </div>
+</div>
+    `
+})
+containerIndustria.innerHTML = fragment;
